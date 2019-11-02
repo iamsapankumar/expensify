2736295726a8d417949e532b86949fc05a634512
Goran7777 <goranilicilke893@gmail.com>
2019-11-02 14:35:57 +0100

    added login component

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import AppRouter, { history } from './routers/AppRouter';
+import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
-import { login, logout } from './actions/auth';
+// import { login, logout } from './actions/auth';
 import 'normalize.css/normalize.css';
+import 'react-dates/lib/css/_datepicker.css';
 import './styles/styles.scss';
 import { app } from './firebase/firebase';
 
@@ -16,30 +17,32 @@ const jsx = (
         <AppRouter />
     </Provider>
 );
-let hasRendered = false;
-const renderApp = () => {
-    if (!hasRendered) {
-        ReactDOM.render(jsx, document.getElementById('app'));
-        hasRendered = true;
-    }
-};
+// let hasRendered = false;
+// const renderApp = () => {
+//     if (!hasRendered) {
+//         ReactDOM.render(jsx, document.getElementById('app'));
+//         hasRendered = true;
+//     }
+// };
 
-ReactDOM.render(<p>Loading ...</p>, document.getElementById('app'));
+ReactDOM.render(jsx, document.getElementById('app'));
 
 
 app.auth().onAuthStateChanged((user) => {
     if (user) {
-        store.dispatch(login(user.uid));
-        store.dispatch(startSetExpenses()).then(() => {
-            renderApp();
-            if (history.location.pathname === '/') {
-                history.push('/dashboard');
-            }
-        });
+        console.log('Log in');
+        // store.dispatch(login(user.uid));
+        // store.dispatch(startSetExpenses()).then(() => {
+        //     renderApp();
+        //     if (history.location.pathname === '/') {
+        //         history.push('/dashboard');
+        //     }
+        // });
     } else {
-        store.dispatch(logout());
-        renderApp();
-        history.push('/');
+        console.log('Log out');
+        // store.dispatch(logout());
+        // renderApp();
+        // history.push('/');
     }
 });
 
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import { Router, Route, Switch } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import NotFoundPage from '../components/NotFoundPage';
+import Header from '../components/Header';
 import LoginPage from '../components/LoginPage';
-import PrivateRoute from './PrivateRoute';
-import PublicRoute from './PublicRoute';
-
-export const history = createBrowserHistory();
 
 const AppRouter = () => (
-    <Router history={history}>
-        <Switch>
-            <PublicRoute exact path='/' component={LoginPage} />
-            <PrivateRoute path='/create' component={AddExpensePage} />
-            <PrivateRoute path='/dashboard' component={ExpenseDashboardPage} />
-            <PrivateRoute path="/edit/:id" component={EditExpensePage} />
-            <Route component={NotFoundPage} />
-        </Switch>
-    </Router>
+    <BrowserRouter>
+        <div>
+            <Header />
+            <Switch>
+                <Route exact path="/" component={LoginPage} />
+                <Route path="/dashboard" component={ExpenseDashboardPage} />
+                <Route path="/create" component={AddExpensePage} />
+                <Route path="/edit/:id" component={EditExpensePage} />
+                <Route component={NotFoundPage} />
+            </Switch>
+        </div>
+    </BrowserRouter>
 );
 
 export default AppRouter;
